3140ff13f5dfbc2a74ef16b075e44c9d79f4b423
francs2812 <emiliofrangella2812@gmail.com>
2021-05-10 16:29:07 +0200

    stampa scheda studente

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var classe = [
+    {
+        nome: "Giulio",
+        cognome: "Franchi",
+        età: 21
+    },
+    {
+        nome: "Marco",
+        cognome: "Saraceni",
+        età: 21
+    }
+];
+
+
+
+
+//stampare scheda studente
+for ( var i=0; i < classe.length; i++) {
+    console.log(classe[i]);
+} 
+
